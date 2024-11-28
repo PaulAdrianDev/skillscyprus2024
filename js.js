849c5ee735af97f70749a820aa77efcbe98791c3
PaulAdrianDev <seicapaul11@gmail.com>
2024-11-28 13:46:05 +0200

    Did header and started Recipe screen

---
+++ js.js
@@ -0,0 +1,37 @@
+function closeOverlay(overlay){
+    overlay.style.opacity = "0";
+    overlay.style.pointerEvents = "none";
+}
+
+function openOverlay(overlay){
+    overlay.style.opacity = "1";
+    overlay.style.pointerEvents = "all";
+}
+
+function openNavOptions(){
+    openOverlay(document.getElementById("navOptions"));
+}
+function closeNavOptions(){
+    closeOverlay(document.getElementById("navOptions"));
+}
+
+setRandomColorToTitle();
+function setRandomColorToTitle(){
+    let options = [
+        '<span style="color: #0bfc03">Flavors</span><br>of the <span style="color:red">World</span>',
+        '<span style="color: #0345fc">Flavors</span><br>of the <span style="color:red">World</span>',
+        'Flavors<br><span style="color:red">of the</span> World',
+    ]
+    let x = Math.floor( Math.random() * options.length );
+
+    document.getElementById("headerTitle").innerHTML = options[x];
+}
+
+setHeightToCarouselOptions();
+function setHeightToCarouselOptions(){
+    let options = document.querySelectorAll(".carouselOption");
+
+    options.forEach((option) =>{
+        option.style.height =  option.querySelector("img").offsetHeight +  option.querySelector("h1").offsetHeight + 20 + "px";
+    });
+}
